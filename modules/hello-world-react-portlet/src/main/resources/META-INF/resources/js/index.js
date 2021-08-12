15f20685761dab08db25d2a1c2e79587fc61dc06
Alessandro Virgilio <alessandro.virgilio@smc.it>
2021-08-12 12:55:52 +0200

    First routing implementation

--- modules/hello-world-react-portlet/src/main/resources/META-INF/resources/js/index.js
+++ modules/hello-world-react-portlet/src/main/resources/META-INF/resources/js/index.js
@@ -2,11 +2,34 @@ import React from 'react';
 
 import DisplayUserInfoComponent from "./display-user-info/DisplayUserInfoComponent";
 import DisplayUserInfoContext from "./display-user-info/DisplayUserInfoContext";
+import NavBar from "./nav-bar/NavBar";
+
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+
+const PortalUsers = () => {
+	return (<h1> PORTAL USERS COMPONENT</h1>)
+}
 
 export default function ({context, props}) {
 	return (
 		<DisplayUserInfoContext.Provider value={context}>
-			<DisplayUserInfoComponent {...props} />
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={() => (
+							<DisplayUserInfoComponent {...props} />
+						)}
+                    />
+                    <Route
+						component={PortalUsers}
+						exact
+						path="/portal-users"
+					/>
+				</Switch>
+			</Router>
 		</DisplayUserInfoContext.Provider>
 	);
 }
