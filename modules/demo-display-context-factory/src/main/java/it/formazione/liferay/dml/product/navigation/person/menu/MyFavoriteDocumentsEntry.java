60b775ced2665005cb762e121aa340e2e45ad097
Alessandro Virgilio <alessandro.virgilio@smc.it>
2021-08-20 12:43:30 +0200

    Added 'My Favorite Documents' Menu Entry

---
+++ modules/demo-display-context-factory/src/main/java/it/formazione/liferay/dml/product/navigation/person/menu/MyFavoriteDocumentsEntry.java
@@ -0,0 +1,34 @@
+package it.formazione.liferay.dml.product.navigation.person.menu;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
+import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
+import it.formazione.liferay.dml.constants.MyCustomContextFactoryPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
+
+/**
+ * @author Virgilio Alessandro 20/ago/2021
+ **/
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.personal.menu.entry.order:Integer=300",
+		"product.navigation.personal.menu.group:Integer=200"
+	},
+	service = PersonalMenuEntry.class
+)
+public class MyFavoriteDocumentsEntry extends BasePersonalMenuEntry {
+
+	@Override
+	public String getPortletId() {
+		return MyCustomContextFactoryPortletKeys.MY_CUSTOM_CONTEXT_FACTORY;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(
+			getResourceBundle(locale), "favorite-documents-menu-entry-label");
+	}
+}
