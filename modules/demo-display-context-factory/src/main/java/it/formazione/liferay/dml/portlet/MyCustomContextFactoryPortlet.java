07a838c00cd8b9ded5bacf26a3b2f4c5acdee2e3
Alessandro Virgilio <alessandro.virgilio@smc.it>
2021-08-21 11:42:55 +0200

    Added portlet to hidden category

--- modules/demo-display-context-factory/src/main/java/it/formazione/liferay/dml/portlet/MyCustomContextFactoryPortlet.java
+++ modules/demo-display-context-factory/src/main/java/it/formazione/liferay/dml/portlet/MyCustomContextFactoryPortlet.java
@@ -14,7 +14,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.formazione",
+		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.false=true",
 		"javax.portlet.display-name=MyCustomContextFactory",
