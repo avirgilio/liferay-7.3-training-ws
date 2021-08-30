3f08af86a15f6928028c2300d0b7622fdea4f294
Alessandro Virgilio <alessandro.virgilio@smc.it>
2021-08-30 23:12:40 +0200

    Added Clay dependency for layout

--- modules/my-todo/my-todo-web/src/main/resources/META-INF/resources/js/App.js
+++ modules/my-todo/my-todo-web/src/main/resources/META-INF/resources/js/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import ClayLayout from "@clayui/layout";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello From React!</h1>
-    </div>
+    <ClayLayout.ContainerFluid view>
+      <ClayLayout.Row justify="center">
+        <h1 style={{ color: "blue" }}>Hello From React!</h1>
+      </ClayLayout.Row>
+    </ClayLayout.ContainerFluid>
   );
 }
