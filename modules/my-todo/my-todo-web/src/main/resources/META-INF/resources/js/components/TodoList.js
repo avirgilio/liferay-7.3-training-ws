b889e21c655a15730c5a4888d2f48f46a35970e5
Alessandro Virgilio <alessandro.virgilio@smc.it>
2021-09-11 11:27:13 +0200

    Stored Todos using useState hook on query completed

--- modules/my-todo/my-todo-web/src/main/resources/META-INF/resources/js/components/TodoList.js
+++ modules/my-todo/my-todo-web/src/main/resources/META-INF/resources/js/components/TodoList.js
@@ -1,14 +1,18 @@
+import React, { useState } from "react";
+
 import { useQuery } from "@apollo/client";
 import ClayList from "@clayui/list";
 import ClayLoadingIndicator from "@clayui/loading-indicator";
-import React from "react";
 import { GET_TODOS_QUERY } from "../graphql/queries/todoQueries";
 import { TodoItem } from "./TodoItem";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
   const { loading, error, data } = useQuery(GET_TODOS_QUERY, {
     fetchPolicy: "network-only",
     nextFetchPolicy: "cache-first",
+    onCompleted: (data) => setTodos(data.todos.items),
   });
 
   if (loading) return <ClayLoadingIndicator />;
@@ -16,7 +20,7 @@ const TodoList = () => {
 
   return (
     <ClayList showQuickActionsOnHover>
-      {data.todos.items.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           id={todo.id}
